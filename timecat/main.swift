fe45a58ab1f180fcb08fe52c021c29b570356ebb
Ryan Brainard <ryan.brainard@gmail.com>
2014-07-20 22:45:15 -0700

    Initial Commit

---
+++ timecat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  timecat
+//
+//  Created by Ryan Brainard on 7/20/14.
+//  Copyright (c) 2014 Ryan Brainard. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
